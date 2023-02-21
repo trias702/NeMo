f1d76b1ea67137b1ef6c5ed29ee0eaedd61ca457
trias702 <daniel.egert@gmail.com>
2023-02-21 09:33:17 -0800

    Small fixes for flashlight decoder

--- nemo/collections/asr/modules/flashlight_decoder.py
+++ nemo/collections/asr/modules/flashlight_decoder.py
@@ -100,13 +100,11 @@ class FlashLightKenLMBeamSearchDecoder(NeuralModule):
 
         try:
             from flashlight.lib.text.dictionary import create_word_dict, load_words
-            from flashlight.lib.sequence.criterion import get_data_ptr_as_bytes
             from flashlight.lib.text.decoder import (
                 CriterionType,
                 LexiconDecoderOptions,
                 KenLM,
                 LM,
-                LMState,
                 SmearingMode,
                 Trie,
                 LexiconDecoder,
